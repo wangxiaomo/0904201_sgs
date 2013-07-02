c6ea80b29ecc89cd2983a4487f209daece3e0586
xiaomo <go.xiaomo@gmail.com>
2013-07-03 01:09:19 +0800

    add general charley

--- 0904201_sgs/extensions/IS.lua
+++ 0904201_sgs/extensions/IS.lua
@@ -2,9 +2,43 @@ module("extensions.IS", package.seeall)
 extension = sgs.Package("IS")
 
 -- 武将: 测试
-test = sgs.General(extension, "test", "wu", "3", false)
+test = sgs.General(extension, "test", "wu", "3", true)
 -- 白板武将
 sgs.LoadTranslationTable{
     ["IS"] = "0904201班级杀",
     ["test"] = "周广禄",
+}
+
+-- 武将: 成昊
+charley = sgs.General(extension, "charley", "shu", 4, true)
+-- 技能: 黑颜
+heiyan = sgs.CreateTriggerSkill{
+    name = "heiyan",
+    events = {sgs.Predamage, sgs.Damage, sgs.Damaged},
+    frequency = sgs.Skill_Compulsory,
+
+    on_trigger = function(self, event, player, data)
+        local room = player:getRoom()
+        local damage = data:toDamage()
+        local card = damage.card
+        if card:isBlack() then
+            if event == sgs.Predamage then
+                damage.damage = damage.damage+1
+                data:setValue(damage)
+                player:speak("黑颜伤害加1")
+            else
+                local recover = sgs.RecoverStruct()
+                recover.who = player
+                recover.reason = self:objectName()
+                recover.recover = 1
+                room:recover(player, recover)
+            end
+        end
+    end,
+}
+charley:addSkill(heiyan)
+sgs.LoadTranslationTable{
+    ["charley"] = "成昊",
+    ["heiyan"] = "黑颜",
+    [":heiyan"] = "使用黑色牌造成的伤害+1,受到黑色牌的伤害-1",
 }
--- readme.md
+++ readme.md
@@ -1,10 +1,25 @@
+项目来源
+==============
+
 火车上无聊读了三国杀的lua extension文档,加上毕业季王亚杰等人的三国杀灵感,决定为0904201各位写一个拓展包.
 技能就来自于班级同学4年来有过的各个梗,比如姚明的激将,成昊同学的黑颜等.
 
-项目安排:
+项目安排
+=============
 
 1. 调试成功拓展包.
 2. 实现部分同学拓展.
 3. 发动班级讨论各个同学技能.
 4. 完全实现.
 5. 发布.
+
+常用接口
+=============
+
+1. sgs.General
+
+  此接口用来创建武将,由太阳神三国杀源码得知General的构造函数为
+
+```cpp
+General(Package *package, const QString &name, const QString &kingdom, int max_hp=4, bool male=true, bool hidden=false, bool never_shown=false)
+```
